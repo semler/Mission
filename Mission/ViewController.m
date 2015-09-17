5bb38490421fa4a76b8a72b55fa6980dac368fbd
于超 <tyou_u@bravesoft.co.jp>
2015-09-17 15:29:19 +0900

    no message

--- Mission/ViewController.m
+++ Mission/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController () {
+@interface ViewController () <UIActionSheetDelegate> {
     __weak IBOutlet UIView *calendarView;
     __weak IBOutlet UILabel *dateLabel;
     __weak IBOutlet UILabel *dayLabel;
@@ -16,6 +16,8 @@
     __weak IBOutlet UIButton *missionButton;
     __weak IBOutlet UIImageView *missionView;
     __weak IBOutlet UILabel *missionMessage;
+    __weak IBOutlet UILabel *clearMessage;
+    __weak IBOutlet UIButton *clearButton;
     __weak IBOutlet UIButton *nextButton;
 }
 
@@ -25,6 +27,7 @@
 - (IBAction)help:(id)sender;
 - (IBAction)next:(id)sender;
 - (IBAction)reset:(id)sender;
+- (IBAction)clear:(id)sender;
 
 @end
 
@@ -36,14 +39,16 @@
     missionButton.hidden = NO;
     missionView.hidden = YES;
     missionMessage.hidden = YES;
+    clearMessage.hidden = YES;
+    clearButton.hidden = YES;
     nextButton.enabled = YES;
     
     NSDate *date = [NSDate date];
     NSCalendar *calendar = [NSCalendar currentCalendar];
     _comps = [calendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitWeekday fromDate:date];
     
-    dateLabel.text = [NSString stringWithFormat:@"%ld.%ld", _comps.year, _comps.month];
-    dayLabel.text = [NSString stringWithFormat:@"%ld", _comps.day];
+    dateLabel.text = [NSString stringWithFormat:@"%ld.%ld", (long)_comps.year, (long)_comps.month];
+    dayLabel.text = [NSString stringWithFormat:@"%ld", (long)_comps.day];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -54,10 +59,12 @@
     missionButton.hidden = YES;
     missionView.hidden = NO;
     missionMessage.hidden = NO;
+    clearMessage.hidden = YES;
+    clearButton.hidden = NO;
     
     NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"misson" ofType:@"plist"];
     NSMutableDictionary *data = [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
-    NSString *key = [NSString stringWithFormat:@"%ld%ld", _comps.month, _comps.day];
+    NSString *key = [NSString stringWithFormat:@"%ld%ld", (long)_comps.month, (long)_comps.day];
     NSString *message = [data objectForKey:key];
     missionMessage.text = message;
 }
@@ -82,18 +89,48 @@
     NSCalendar *calendar = [NSCalendar currentCalendar];
     _comps = [calendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitWeekday fromDate:date];
     
-    dateLabel.text = [NSString stringWithFormat:@"%ld.%ld", _comps.year, _comps.month];
-    dayLabel.text = [NSString stringWithFormat:@"%ld", _comps.day];
+    dateLabel.text = [NSString stringWithFormat:@"%ld.%ld", (long)_comps.year, (long)_comps.month];
+    dayLabel.text = [NSString stringWithFormat:@"%ld", (long)_comps.day];
     
     missionButton.hidden = NO;
     missionView.hidden = YES;
     missionMessage.hidden = YES;
+    clearMessage.hidden = YES;
+    clearButton.hidden = YES;
+}
+
+- (IBAction)clear:(id)sender {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc]
+                                  initWithTitle:nil
+                                  delegate:self
+                                  cancelButtonTitle:nil
+                                  destructiveButtonTitle:nil
+                                  otherButtonTitles:@"アプリに達成報告", @"アプリに証拠付きで達成報告", @"SNSに証拠付きで達成報告", nil];
+    
+    actionSheet.actionSheetStyle = UIBarStyleBlackTranslucent;
+    [actionSheet showInView:self.view];
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 0) {
+        clearMessage.hidden = NO;
+        clearButton.hidden = YES;
+    } else if (buttonIndex == 1) {
+        clearMessage.hidden = NO;
+        clearButton.hidden = YES;
+    } else if (buttonIndex == 2) {
+        clearMessage.hidden = NO;
+        clearButton.hidden = YES;
+    }
 }
 
 -(void) update {
     missionButton.hidden = NO;
     missionView.hidden = YES;
     missionMessage.hidden = YES;
+    clearButton.hidden = YES;
+    clearMessage.hidden = YES;
 
     NSCalendar *calendar = [NSCalendar currentCalendar];
     NSDate *date =  [calendar dateFromComponents:_comps];
@@ -111,8 +148,8 @@
             _comps.day = 1;
         }
     }
-    dateLabel.text = [NSString stringWithFormat:@"%ld.%ld", _comps.year, _comps.month];
-    dayLabel.text = [NSString stringWithFormat:@"%ld", _comps.day];
+    dateLabel.text = [NSString stringWithFormat:@"%ld.%ld", (long)_comps.year, (long)_comps.month];
+    dayLabel.text = [NSString stringWithFormat:@"%ld", (long)_comps.day];
 }
 
 @end
